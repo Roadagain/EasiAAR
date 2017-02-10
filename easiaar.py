373a0292dc28201f38bc755e2628e1e9b6146ec4
Roadagain <ringoh72@gmail.com>
2017-02-11 00:56:41 +0900

    正規表現のパターン文字列を全部正規表現オブジェクトに変更
    後ろのマッチで余計な生成しなくて済む

--- easiaar.py
+++ easiaar.py
@@ -16,22 +16,23 @@ for line in open(sys.argv[1]):
     before, after = line.split()
     dictionary[before] = after
 
-QUOTED = r'".*?"'
-QUOTE_AS_IS = r'[!]' + QUOTED
-OTHERWISE = r'\S+'
-SPLIT_PATTERN = re.compile('|'.join([QUOTE_AS_IS, QUOTED, OTHERWISE]))
+QUOTED = re.compile(r'".*?"')
+QUOTE_AS_IS = re.compile(r'[!]' + QUOTED.pattern)
+OTHERWISE = re.compile(r'\S+')
+WORDS = '|'.join([QUOTE_AS_IS.pattern, QUOTED.pattern, OTHERWISE.pattern])
+SPLIT_PATTERN = re.compile(WORDS)
 
 for line in sys.stdin:
     translated = []
     for word in SPLIT_PATTERN.findall(line):
-        unquoted = word.strip('"')
-        is_banged = (QUOTE_AS_IS.match(word) is not None)
+        is_quoted = QUOTED.match(word) is not None
+        is_banged = QUOTE_AS_IS.match(word) is not None
         if word in dictionary:
             translated.append(dictionary[word])
         elif is_banged:
             translated.append(word[1:])
-        elif word != unquoted:
-            zipped = translate_words(unquoted.split(), dictionary, '')
+        elif is_quoted:
+            zipped = translate_words(word.strip('"').split(), dictionary, '')
             translated.append(zipped)
             dictionary[word] = zipped
         else:
